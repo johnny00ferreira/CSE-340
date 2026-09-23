ab01ebd552c2b31c09de8975cba9af49ead6ea92
johnny00ferreira <johnnyferreiradasilva7@gmail.com>
2026-09-23 20:22:21 -0300

    Add service project details and upcoming projects

--- src/models/projects.js
+++ src/models/projects.js
@@ -41,5 +41,57 @@ const getProjectsByOrganizationId = async (organizationId) => {
     return result.rows;
 };
 
-export { getAllProjects, getProjectsByOrganizationId };
+const getUpcomingProjects = async (numberOfProjects) => {
+    const query = `
+        SELECT
+            p.project_id,
+            p.title,
+            p.description,
+            p.date,
+            p.location,
+            p.organization_id,
+            o.name AS organization_name
+        FROM project p
+        JOIN organization o
+            ON p.organization_id = o.organization_id
+        WHERE p.date >= CURRENT_DATE
+        ORDER BY p.date ASC
+        LIMIT $1;
+    `;
+
+    const queryParams = [numberOfProjects];
+    const result = await db.query(query, queryParams);
+
+    return result.rows;
+};
+
+const getProjectDetails = async (id) => {
+    const query = `
+        SELECT
+            p.project_id,
+            p.title,
+            p.description,
+            p.date,
+            p.location,
+            p.organization_id,
+            o.name AS organization_name
+        FROM project p
+        JOIN organization o
+            ON p.organization_id = o.organization_id
+        WHERE p.project_id = $1;
+    `;
+
+    const queryParams = [id];
+    const result = await db.query(query, queryParams);
+
+    return result.rows.length > 0 ? result.rows[0] : null;
+};
+
+
+export {
+    getAllProjects,
+    getProjectsByOrganizationId,
+    getUpcomingProjects,
+    getProjectDetails
+};
 
--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,35 @@
 import express from 'express';
 
 import { showHomePage } from './controllers/index.js';
+
 import {
     showOrganizationsPage,
     showOrganizationDetailsPage
 } from './controllers/organizations.js';
-import { showProjectsPage } from './controllers/projects.js';
+
+import {
+    showProjectsPage,
+    showProjectDetailsPage
+} from './controllers/projects.js';
+
 import { showCategoriesPage } from './controllers/categories.js';
 import { testErrorPage } from './controllers/errors.js';
 
 const router = express.Router();
 
 router.get('/', showHomePage);
-router.get('/organizations', showOrganizationsPage);
-router.get('/projects', showProjectsPage);
-router.get('/categories', showCategoriesPage);
 
-// Route for organization details page
+router.get('/organizations', showOrganizationsPage);
+
 router.get('/organization/:id', showOrganizationDetailsPage);
 
-// error-handling routes
+router.get('/projects', showProjectsPage);
+
+router.get('/project/:id', showProjectDetailsPage);
+
+router.get('/categories', showCategoriesPage);
+
+// Error-handling route
 router.get('/test-error', testErrorPage);
 
 export default router;
